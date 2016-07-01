b1ce3d83a8fa518d46eead6cd87efc0966a4d0c7
Steven Myers <slmyers@ualberta.ca>
2016-06-30 22:37:11 -0600

    got routes working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ROUTER_DIRECTIVES } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'app/home/home.component.html',
-  styleUrls: ['app/home/home.component.css']
+  styleUrls: ['app/home/home.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class HomeComponent implements OnInit {
 
