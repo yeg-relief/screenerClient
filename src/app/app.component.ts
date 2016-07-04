30d47340606408f16d05eff20b2972f801557ff8
Steven Myers <slmyers@ualberta.ca>
2016-07-04 17:14:38 -0600

    added router directives to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { MediaListener } from './media-listener.directive';
 import { NavbarComponent } from './navbar/navbar.component'; 
 import { Store } from '@ngrx/store';
 import { AppState } from './reducers';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 
 @Component({
   selector: 'my-app',
   templateUrl:'app/app.component.html',
-  directives: [NavbarComponent, MediaListener]
+  directives: [NavbarComponent, MediaListener, ROUTER_DIRECTIVES]
 })
 export class AppComponent implements OnInit{
   
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import { MasterScreenerComponent } from './master-screener/index';
-import { HomeComponent } from './home/home.component';
+import { MasterScreenerComponent } from './master-screener';
+import { HomeComponent } from './home';
 
 export const routes: RouterConfig = [
   { path: '', component: HomeComponent },
