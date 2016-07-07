21543185ad4f5a927e7ab2e3a7366116b8d39919
Steven Myers <slmyers@ualberta.ca>
2016-07-07 13:59:03 -0600

    404 edit

--- src/app/404/404.component.ts
+++ src/app/404/404.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ycb-404',
-  template: `<div style="margin-top:25%; margin-left:30%;"><b> THIS ROUTE IS NOT IMPLEMENTED</b></div>`
+  template: `<div style="margin-top:25%; margin-left:30%;"><b> THIS ROUTE IS NOT IMPLEMENTED</b></div>
+             <div style="margin-top:25%; margin-left:30%;"><b> THIS ROUTE IS NOT IMPLEMENTED</b></div>
+             <div style="margin-top:25%; margin-left:30%;"><b> THIS ROUTE IS NOT IMPLEMENTED</b></div>`
 })
 export class FourOhFourComponent{}
