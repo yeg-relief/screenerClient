bd060b47c2b109235ce855db62b8c42218f1fb38
Steven Myers <slmyers@ualberta.ca>
2016-07-15 14:36:28 -0600

    clean up route config imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import { MasterScreenerComponent } from './pages/index';
-import { HomeComponent } from './pages/index';
-import { FourOhFourComponent } from './pages/index';
-import { MasterScreenerResultsComponent } from './pages/index';
+import { 
+  MasterScreenerComponent, HomeComponent,
+  FourOhFourComponent, MasterScreenerResultsComponent
+} from './pages';
 
 export const routes: RouterConfig = [
   { path: '', component: HomeComponent },
