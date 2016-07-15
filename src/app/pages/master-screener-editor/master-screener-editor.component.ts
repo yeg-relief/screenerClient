902f7b07f27ad2547ca91b305c166a5931fd5d40
Steven Myers <slmyers@ualberta.ca>
2016-07-15 14:21:27 -0600

    switching to editor branch

---
+++ src/app/pages/master-screener-editor/master-screener-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ycb-ms-editor',
+  template: '',
+  styles: [''],
+  directives: [],
+  providers: []
+})
+export class MasterScreenerEditor{
+  
+}
