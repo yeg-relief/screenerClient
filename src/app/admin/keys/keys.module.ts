415b6ed4b1bdb8393950cc4e65990129cc4c6991
Steven Myers <slmyers@ualberta.ca>
2017-02-25 16:29:52 -0700

    added key filter

--- src/app/admin/keys/keys.module.ts
+++ src/app/admin/keys/keys.module.ts
@@ -4,7 +4,6 @@ import { KeysComponent } from './keys.component';
 import { KeysOverviewComponent } from './overview/overview.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@angular/material';
-import { KeyDetailComponent } from './detail/detail.component';
 import { KeyEditComponent } from './edit/key-edit.component';
 import { KeyOverviewControlsComponent } from './overview/key-overview-controls/key-overview-controls.component';
 import { AdminCoreModule } from '../core/admin-core.module';
@@ -22,7 +21,6 @@ import { KeyResolverService } from './overview/key-resolver.service';
   declarations: [
     KeysComponent,
     KeysOverviewComponent,
-    KeyDetailComponent,
     KeyEditComponent,
     KeyOverviewControlsComponent
   ],
--- src/app/admin/keys/overview/overview.component.ts
+++ src/app/admin/keys/overview/overview.component.ts
@@ -6,6 +6,12 @@ import * as fromRoot from '../../reducer';
 import { Observable } from 'rxjs/Observable';
 import { DataService } from '../../data.service';
 import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/let';
+import 'rxjs/add/observable/merge';
+
 
 @Component({
   selector: 'app-keys-overview',
@@ -14,7 +20,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class KeysOverviewComponent implements OnInit {
   loadedKeys$: Observable<Key[]>;
-
+  private filter = new BehaviorSubject<string>('');
   constructor(private store: Store<fromRoot.State>, private data: DataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -25,6 +31,42 @@ export class KeysOverviewComponent implements OnInit {
     } else {
       this.store.dispatch(new keysActions._LoadKeysFailure({}));
     }
-    this.loadedKeys$ = this.store.let(fromRoot.allLoadedKeys);
+
+    this.loadedKeys$ = Observable.merge(
+      this.store.let(fromRoot.allLoadedKeys),
+      this.filter
+    )
+      .scan( (state: KeyFilterState, update: string | Key[]) => {
+        if (Array.isArray(update)) {
+          state.keys = [...update];
+        } else if ( typeof update === 'string') {
+          state.keyName = update;
+        }
+
+        return state;
+      }, new KeyFilterState([], ''))
+      .let(applyFilter)
+      
   }
+
+  handleFilter(keyName: string) {
+    this.filter.next(keyName);
+  }
+}
+
+class KeyFilterState {
+  keys: Key[];
+  keyName: string;
+
+  constructor(keys, keyName) {
+    this.keys = keys;
+    this.keyName = keyName;
+  }
+}
+
+function applyFilter(source: Observable<KeyFilterState>): Observable<Key[]> {
+  return source.switchMap( (state: KeyFilterState) => {
+    const regexp = new RegExp(state.keyName);
+    return Observable.of(state.keys.filter(k => regexp.test(k.name)))
+  })
 }
