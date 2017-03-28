50d7b5547d49c1a85e13951531b72fe6e35d7574
Steven Myers <slmyers@ualberta.ca>
2017-03-27 21:17:25 -0600

    (chore) update styling for admin/keys

--- src/app/admin/keys/keys.component.ts
+++ src/app/admin/keys/keys.component.ts
@@ -3,16 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   template:
   `
-    <md-card class="col col-12 flex flex-column overview-wrapper">
+    <md-card class="col-10 flex flex-column overview-wrapper">
       <router-outlet></router-outlet>
     </md-card>
   `,
   styles: [`
     md-card {
-      height: 80vh;
+      height: 60vh;
       overflow-x: hidden;
-      overflow-y: auto;
-      font-family: 'Roboto Mono', monospace;
+      overflow-y: scroll;
+      font-family: 'Open Sans', sans-serif;
+      margin: 0 auto;
     }
   `]
 })
