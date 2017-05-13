423a90a5c97f09ae65c5f7dea11b80a42fe7a17b
Steven Myers <slmyers@ualberta.ca>
2017-05-13 10:01:01 -0600

    updated app dependencies and worked on program overview controls

--- src/app/admin/programs/program-overview/overview-controls/overview-controls.component.ts
+++ src/app/admin/programs/program-overview/overview-controls/overview-controls.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Animations } from '../../../../shared/animations';
+
 
 @Component({
   selector: 'app-overview-controls',
   templateUrl: './overview-controls.component.html',
-  styleUrls: ['./overview-controls.component.css']
+  styleUrls: ['./overview-controls.component.css'],
+  animations: [
+    Animations.fadeinAndOut
+  ]
 })
 export class OverviewControlsComponent implements OnInit, OnDestroy {
   @Input() loaded: boolean;
   @Input() deleteInitiated: boolean;
   @Output() onFilter = new EventEmitter<any>();
   form: FormGroup;
+  filter = "";
   subscription;
   constructor(private fb: FormBuilder) { }
 
@@ -22,6 +28,7 @@ export class OverviewControlsComponent implements OnInit, OnDestroy {
 
     this.subscription = this.form.valueChanges
       .filter( _ => this.form.valid )
+      .do( filter => this.filter = filter.type )
       .subscribe(filter => this.onFilter.emit(filter));
   }
 
