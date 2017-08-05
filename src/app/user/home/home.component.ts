aaa85ff48eb555d15e8f330ca891f6bb538d82ac
Steven Myers <slmyers@ualberta.ca>
2017-08-04 21:50:48 -0600

    redo homescreen template/styles

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
+import { Component } from '@angular/core';
 import {
   trigger,
   state,
@@ -22,28 +21,4 @@ import {
     ])
   ]
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  loading = false;
-  timeout;
-
-  constructor(private router: Router){}
-
-  ngOnInit(){
-    const questionsUrl = '/master-screener/questions';
-    const adminUrl = '/admin/master-screener/overview';
-
-    this.router.events
-      .subscribe(
-        event => {
-          if(event instanceof NavigationStart && (event.url === questionsUrl || event.url === adminUrl) ) {
-            this.timeout = setTimeout(() => this.loading = true, 100)
-            this.loading = true;
-          }
-        }
-      );
-  }
-
-  ngOnDestroy(){
-    clearTimeout(this.timeout);
-  }
-}
+export class HomeComponent {}
