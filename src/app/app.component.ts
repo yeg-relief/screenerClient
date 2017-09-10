33b3d9ca71cc077d43b0f7bc2ae85d88e1956adf
Steven Myers <slmyers@ualberta.ca>
2017-09-10 14:26:51 -0600

    style changes for browse program

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,33 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, NavigationStart, NavigationEnd } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
-import { AuthService } from './admin/core/services/auth.service';
-import { AuthGuardService } from './admin/core/services/auth-guard.service'
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/multicast';
 
 
 @Component({
-  selector: 'app-root',
-  template: `
+    selector: 'app-root',
+    template: `
     <app-toolbar></app-toolbar>
     <main id="main-outlet" [ngClass]="backgroundClass">
       <router-outlet></router-outlet>
     </main>
   `,
-  styles: [
-    `
+    styles: [
+        `
       #main-outlet {
         min-height: 94vh;
-        width: 98vw;
+        width: 100vw;
         margin: 0 auto;
+        overflow-x: hidden;
       }
-    `,
-    `
+    `,`
       .background {
         background-image: url(assets/17_03_15-YouCanBenefit-Illustration.svg);
         background-position: center bottom;
         background-repeat: no-repeat;
         background-size: contain;
       }
-    `,
-    `
+    `,`
       @media(max-width: 600px) {
         .background {
           background-image: url(assets/17_03_15-YouCanBenefit-Illustration.svg);
@@ -41,47 +36,46 @@ import 'rxjs/add/operator/multicast';
           background-size: cover;
         }
       }
-    `,
-    `
+    `,`
       .backgroundcolor {
         background-color: lightgoldenrodyellow;
       }
     `
 
-  ],
-  providers: [ ]
+    ],
+    providers: [ ]
 })
 export class AppComponent implements OnInit {
-  isIE: boolean;
-  backgroundClass = {
-    background: true,
-    backgroundcolor: false 
-  };
+    isIE: boolean;
+    backgroundClass = {
+        background: true,
+        backgroundcolor: false
+    };
 
-  constructor(private router: Router){}
+    constructor(private router: Router){}
 
-  ngOnInit() {
+    ngOnInit() {
 
-    const userAgent: string = window ? window.navigator.userAgent: '';
-    this.isIE = userAgent.includes('Trident');
+        const userAgent: string = window ? window.navigator.userAgent: '';
+        this.isIE = userAgent.includes('Trident');
 
-    const isAdminRoute = url => url.substring(0, 7) === '/admin/';
+        const isAdminRoute = url => url.substring(0, 7) === '/admin/';
 
-    this.router.events
-      .map(event => event instanceof NavigationEnd ? this.router.url : undefined)
-      .filter(url => url !== undefined)
-      .debounceTime(60)
-      .map( url => isAdminRoute(url) )
-      .subscribe( val => {
-        if (this.isIE) {
-            this.backgroundClass.background = false;
-            this.backgroundClass.backgroundcolor = true;
-        } else {
-            this.backgroundClass.background = !val;
-            this.backgroundClass.backgroundcolor = val;
-        }
+        this.router.events
+            .map(event => event instanceof NavigationEnd ? this.router.url : undefined)
+            .filter(url => url !== undefined)
+            .debounceTime(60)
+            .map( url => isAdminRoute(url) )
+            .subscribe( val => {
+                if (this.isIE) {
+                    this.backgroundClass.background = false;
+                    this.backgroundClass.backgroundcolor = true;
+                } else {
+                    this.backgroundClass.background = !val;
+                    this.backgroundClass.backgroundcolor = val;
+                }
 
 
-      });
-  }
+            });
+    }
 }
