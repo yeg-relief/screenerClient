91b44e78845a0edea5c3e004b484b8d641cf225b
Steven Myers <slmyers@ualberta.ca>
2017-10-04 20:48:19 -0600

    increase tag max-length

--- src/app/admin/programs/program-edit/tag-edit/tag-edit.component.ts
+++ src/app/admin/programs/program-edit/tag-edit/tag-edit.component.ts
@@ -2,25 +2,25 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormArray, FormControl, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-tag-edit',
-  templateUrl: './tag-edit.component.html',
-  styleUrls: ['./tag-edit.component.css']
+    selector: 'app-tag-edit',
+    templateUrl: './tag-edit.component.html',
+    styleUrls: ['./tag-edit.component.css']
 })
 export class TagEditComponent implements OnInit {
-  @Input() tags: FormArray;
-  linkInput = new FormControl('', Validators.pattern('[a-zA-Z0-9]{2,8}'))
-  constructor() { }
+    @Input() tags: FormArray;
+    linkInput = new FormControl('', Validators.pattern('[a-zA-Z0-9]{2,32}'));
+    constructor() { }
 
-  ngOnInit() {}
+    ngOnInit() {}
 
-  addTag() {
-    if (this.linkInput.valid && this.linkInput.value.trim().length > 0 ) {
-      this.tags.push(new FormControl(this.linkInput.value));
-      this.linkInput.setValue('');
+    addTag() {
+        if (this.linkInput.valid && this.linkInput.value.trim().length > 0 ) {
+            this.tags.push(new FormControl(this.linkInput.value));
+            this.linkInput.setValue('');
+        }
     }
-  }
 
-  removeTagAt(index: number){
-    this.tags.removeAt(index);
-  }
+    removeTagAt(index: number){
+        this.tags.removeAt(index);
+    }
 }
