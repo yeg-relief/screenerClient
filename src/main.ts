5f9939efa51eab4a91036578d61d4c4e37c0aaf9
Steven Myers <slmyers@ualberta.ca>
2018-03-21 16:38:24 -0700

    uncomment analytics code...

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import './polyfills.ts';
-// import 'autotrack';
-// import 'autotrack/lib/plugins/event-tracker';
-// import 'autotrack/lib/plugins/url-change-tracker';
+import 'autotrack';
+import 'autotrack/lib/plugins/event-tracker';
+import 'autotrack/lib/plugins/url-change-tracker';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/';
 const tracking_id = process.env.ANALYTICS_ID || 'UA-110454771-1';
-/*
 const tracking_script_src = process.env.NODE_ENV === 'production'
     ?   'https://www.google-analytics.com/analytics.js'
     :   'https://www.google-analytics.com/analytics_debug.js';
 require('./analytics.js')(tracking_id, tracking_script_src, false);
-*/
+
 if (environment.production) {
     enableProdMode();
 }
